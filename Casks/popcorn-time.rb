a98e1c78df845b0e82dc6ee8ebcbfae60443e6f2
Oskar Schöldström <oskar.scholdstrom@arcada.fi>
2015-06-19 23:43:18 -0400

    Update popcorn-time direct url

--- Casks/popcorn-time.rb
+++ Casks/popcorn-time.rb
@@ -1,8 +1,7 @@
 cask :v1 => 'popcorn-time' do
   version '0.3.7-2'
   sha256 '8fb9c3be5ec5600084745885f0f6b2f948fa1828983fe8cb7186f7144447bb8f'
-
-  url "https://get.popcorntime.io/build/Popcorn-Time-#{version.gsub('-', '.')}-Mac.dmg", :user_agent => :fake
+  url "https://104.131.187.115/build/Popcorn-Time-#{version.gsub('-', '.')}-Mac.dmg", :user_agent => :fake
   appcast 'https://popcorntime.io/update.json',
     :sha256 => 'a9bd0f4938c7bd1a8a3e42efa32eafa9a97f2b9100283b7a15a4fc5c6213cbc7',
     :format => :unknown
